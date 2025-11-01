0a354b52f7fd8dda0539cd48da4678118bf87c8e
Ovais Rashid <159573241+Ovaserashid@users.noreply.github.com>
2025-11-01 23:39:53 +0530

    Train and predict with regressor model
    Trained the same dataset with a regressor model
    (RandomForestRegressor) to predict closing price.
    Score is too good (0.99) hence there might be
    data leakage. Need to investigate further.

--- data/extract.py
+++ data/extract.py
@@ -2,11 +2,11 @@ import yfinance as yf
 from datetime import date
 from datetime import datetime
 SENSEX = yf.Ticker("^BSESN")
-def extract_data(start, end, interval):
+def extract_data(start, end):
     if end == datetime.now().year:
         end = date.today()
         try:
-            SENSEXDATA = SENSEX.history(start=f"{start}-01-01", end=end, interval=interval)
+            SENSEXDATA = SENSEX.history(start=f"{start}-01-01", end=end)
         except Exception as e:
             print(f"Error fetching data for {start} to {end}: {e}")
             SENSEXDATA = None
@@ -14,7 +14,7 @@ def extract_data(start, end, interval):
             pass
     else:
         try:
-            SENSEXDATA = SENSEX.history(start=f"{start}-01-01", end=f"{end}-12-31", interval=interval)
+            SENSEXDATA = SENSEX.history(start=f"{start}-01-01", end=f"{end}-12-31")
         except Exception as e:
             print(f"Error fetching data for {start} to {end}: {e}")
             SENSEXDATA = None
